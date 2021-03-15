c4993cb30465bc01d6707e59b780391b210fbcaf
Sembenevi <guilherme.sembeneli@gmail.com>
2021-03-15 10:57:35 -0300

    challenge completed

---
+++ README.md
@@ -0,0 +1,42 @@
+# To.do
+
+# About this Project
+
+- The "watchme" is a challenge created by rocketseat to train composing
+- I created an "intefarce" folder to store the typescript types and separated the application into components
+
+# Technologies and Tools that were used
+
+- Webpack
+- Babel
+- Typescript
+- React.JS
+- Yarn
+- Sass
+
+# Install this Repository
+
+1. **Run the application**
+
+- First clone this repository:
+
+```bash
+ git clone https://github.com/Sembenevi/challenge-watchme-ignite.git
+```
+
+2. **You need install dependecies been use in this project**
+
+```bash
+    yarn install
+```
+
+3. To been execute:
+
+```bash
+yarn dev
+```
+
+- This project is been run in **http://localhost:8080/**
+
+
+**Made in @Rocketseat Ignite's complementary challenge **
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
+import React from "react";
+import { GenreResponseProps } from "../interface/interfaces";
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
 }
+
+export const SideBar: React.FC<SideBarProps> = ({
+  genres,
+  selectedGenreId,
+  setSelectedGenreId,
+}) => {
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div className="buttons-container">
+      {genres.map((genre) => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  );
+};
